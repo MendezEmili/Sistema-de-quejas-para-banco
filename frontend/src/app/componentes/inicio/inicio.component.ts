687d3b6629af58f74d8bb0b0f4e727105dd274e8
shoberto <hrobert.2897@gmail.com>
2020-05-08 21:15:30 -0600

    Login completo

--- frontend/src/app/componentes/inicio/inicio.component.ts
+++ frontend/src/app/componentes/inicio/inicio.component.ts
@@ -4,17 +4,34 @@ import { Component, OnInit } from '@angular/core';
 import { CatalogosService } from '../../servicios/catalogos.service'
 import { Router } from '@angular/router';
 
+//importar modelos
+import {Usuarios} from '../../modelos/usuarios';
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
+
 export class InicioComponent implements OnInit {
 
   autorizacion: boolean;
   token: any;
 
+  usuario : Usuarios = {
+    dpi: 0,
+    nombre_usuario: '',
+    correo_usuario: '',
+    cargo_usuario: '',
+    region: 0,
+    id_puntosdeatencion: 0,
+    estado_usuario: 1,
+    password:'',
+    fecha_creacion: new Date()
+    
+  }
   constructor(private catalogosService: CatalogosService, private ruta: Router) { }
+
 
   ngOnInit(): void {
     this.autorizacion = false;
@@ -25,8 +42,17 @@ export class InicioComponent implements OnInit {
   }
 
   login(){
-    this.catalogosService.setToken("valido");
-    location.reload();    
+    this.catalogosService.login(this.usuario.correo_usuario, this.usuario.password).subscribe(
+      res=> {
+        this.catalogosService.setToken("valido");
+        location.reload();    
+      }, 
+      err=> {
+        alert("Usuario No Encontrado");
+      }
+    )
+   
+   
   }
 
   logout(){
