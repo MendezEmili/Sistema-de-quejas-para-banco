51df0b75e987b6ca4aad8a0fa56aaed75cd4381d
raquel mendez <raquel_mendez@live.com>
2020-05-22 15:14:06 -0600

    Generacion del primer reporte

--- frontend/src/app/componentes/reporte/reporte.component.ts
+++ frontend/src/app/componentes/reporte/reporte.component.ts
@@ -3,7 +3,7 @@ import { CatalogosService } from 'src/app/servicios/catalogos.service';
 import { Router } from '@angular/router';
 
 //Reportes PDF
-import { PdfMakeWrapper, Txt } from 'pdfmake-wrapper';
+import { PdfMakeWrapper, Txt, Table, Columns } from 'pdfmake-wrapper';
 
 @Component({
   selector: 'app-reporte',
@@ -12,6 +12,7 @@ import { PdfMakeWrapper, Txt } from 'pdfmake-wrapper';
 })
 export class ReporteComponent implements OnInit {
   token: any;
+  queja: any = [];
 
   constructor(private catalogosService: CatalogosService, private ruta: Router) { }
 
@@ -27,20 +28,32 @@ export class ReporteComponent implements OnInit {
   generarPDF(){
     const pdf = new PdfMakeWrapper();
     pdf.header(
-      new Txt('REPORTE DE QUEJAS POR MAL SERVICIO O SERVICIO NO CONFORME').bold().end
+      new Txt('REPORTE DE QUEJAS POR MAL SERVICIO O SERVICIO NO CONFORME').bold().alignment('center').end
     )
 
     pdf.footer(
-      new Txt('Universidad Mariano Gálvez -2020- Roberto Marín && Emili Méndez').end
+      new Txt('Universidad Mariano Gálvez -2020- Roberto Marín && Emili Méndez').alignment('center').end
     )
-
-    pdf.pageMargins([ 80, 60, 40, 60 ]);
-
+    pdf.pageOrientation('landscape');
+    pdf.pageMargins(40)
     pdf.add(
-      new Txt('Hola Mundo').end
+      new Table([
+        ['No. ', 'Tipo', 'Punto de atención', 'Estado', 'Etapa', 'Resultado', 'Medio de ingreso', 'Fecha de creación', 'Tiempo de atención', 'Detalle']
+      ]).end
     );
-
-    pdf.create().open();
+    this.catalogosService.reporteNoQueja("QMS", 6, 2020).subscribe(
+      res=>{
+        console.log(res);
+        this.queja = res;   
+        for(let i = 0; i<this.queja.length; i++){
+          pdf.add(
+            new Table([[this.queja[i].id_queja, this.queja[i].tipo_queja, this.queja[i].nombre_puntodeatencion, this.queja[i].estado, this.queja[i].estado_i, this.queja[i].descripcion_medio, this.queja[i].resultado, this.queja[i].fecha_ingreso, this.queja[i].detalle_queja]]).end
+          )
+        }     
+        pdf.create().open();
+      }
+    )
+    
   }
 
 }
